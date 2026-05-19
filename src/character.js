5a6d5462e9804f0612ad17074c1077cdb89764f3
Osei Baah <obaah9@gmail.com>
2026-05-19 14:15:39 +0000

    update character

--- src/character.js
+++ src/character.js
@@ -279,10 +279,10 @@ export class CharacterController {
       if (this.keys['KeyD'] || this.keys['ArrowRight']) moveDir.add(right);
 } else {
   
-      if (this.keys['KeyS'] || this.keys['ArrowUp'])    moveDir.add(forward);
-      if (this.keys['KeyW'] || this.keys['ArrowDown'])  moveDir.sub(forward);
-      if (this.keys['KeyD'] || this.keys['ArrowLeft'])  moveDir.sub(right);
-      if (this.keys['KeyA'] || this.keys['ArrowRight']) moveDir.add(right);
+      if (this.keys['KeyS'] || this.keys['ArrowDown'])    moveDir.add(forward);
+      if (this.keys['KeyW'] || this.keys['ArrowUp'])  moveDir.sub(forward);
+      if (this.keys['KeyD'] || this.keys['ArrowRight'])  moveDir.sub(right);
+      if (this.keys['KeyA'] || this.keys['ArrowLeft']) moveDir.add(right);
 }
 
     const isMoving = moveDir.lengthSq() > 0;
